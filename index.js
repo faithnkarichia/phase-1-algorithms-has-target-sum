b3b52b353f2a32c5e304247a33e73ab2c6151ffb
Faith <kashfaith77@gmail.com>
2025-04-03 17:56:46 +0300

    Apply problem solving

--- index.js
+++ index.js
@@ -1,22 +1,36 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  let seenNumbers = new Set();
+  for (let num of array) {
+      let complement = target - num;
+      if (seenNumbers.has(complement)) {
+          return true;
+      }
+      seenNumbers.add(num);
+  }
+  return false;
 }
 
 /* 
   Write the Big O time complexity of your function here
+  O(n)
 */
 
 /* 
-  Add your pseudocode here
+create an empty set to store numbers
+loop through all the numbers
+do target -the numbers looped through and check if the answer is in the set
+if so return true else return false
 */
 
 /*
-  Add written explanation of your solution here
+  
 */
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([2,4,6,7], 10));
   console.log("Expecting: true");
   console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 10));
 
